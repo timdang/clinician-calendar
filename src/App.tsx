236dc9539ae0f9d56c3091f631d2d28b828ff7c6
Timothy Dang <timothy.dang@guaranteedrate.com>
2022-01-23 16:01:37 -0800

    some logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import React, { useState } from "react";
 
 import AddClinician from "./components/AddClinician";
 import { Clinician } from "./features/types";
+import { mapDays } from "./features/utilities";
+import { last } from "lodash-es";
 
 function App() {
   const [clinicians, setClinicians] = useState<Clinician[]>([]);
@@ -12,7 +14,7 @@ function App() {
     <div className="App">
       <h1 className="text-3xl font-bold py-4 underline">Clinician Calendar</h1>
       <button
-        className="rounded-lg px-4 py-2 m-2 bg-blue-100 text-blue-800"
+        className="rounded-lg px-4 py-2 m-2 bg-blue-800 text-blue-100"
         type="button"
         onClick={() => setShowAdd(true)}
       >
@@ -29,7 +31,17 @@ function App() {
         />
       )}
       {clinicians.map((clinician) => (
-        <div key={clinician.startDate}>{clinician.firstName}</div>
+        <div key={`${clinician.lastName}${clinician.startDate}`}>
+          <span
+            className={`font-bold text-${clinician.color}-600`}
+            style={{ color: clinician.color }}
+          >
+            {clinician.firstName} {clinician.lastName}
+          </span>
+          {last(
+            mapDays(clinician, 7, ["01/17/2022", "01/18/2022"], ["01/01/2022"])
+          )}
+        </div>
       ))}
     </div>
   );
--- src/features/utilities.ts
+++ src/features/utilities.ts
@@ -1,3 +1,6 @@
+import { addBusinessDays, isSameDay } from "date-fns";
+import { forEach, last } from "lodash-es";
+
 import { Clinician } from "./types";
 
 export const mapDays = (
@@ -7,5 +10,23 @@ export const mapDays = (
   holidaySchedule: string[]
 ): string[] => {
   const days: string[] = [];
+  while (trainingDays) {
+    const previousTrainingDay = last(days);
+    let nextDay = previousTrainingDay
+      ? addBusinessDays(new Date(previousTrainingDay), 1)
+      : new Date(`${clinician.startDate} 00:00:00 PST`);
+    forEach(holidaySchedule, (holiday: string) => {
+      if (isSameDay(new Date(`${holiday} 00:00:00 PST`), nextDay)) {
+        nextDay = addBusinessDays(nextDay, 1);
+      }
+    });
+    forEach(vacationSchedule, (vacationDay: string) => {
+      if (isSameDay(new Date(`${vacationDay} 00:00:00 PST`), nextDay)) {
+        nextDay = addBusinessDays(nextDay, 1);
+      }
+    });
+    days.push(nextDay.toDateString());
+    trainingDays--;
+  }
   return days;
 };
